31c1bb1df7ff00585c38476dd0de18cb4b15c368
Max DeVos <devosmaxwell@gmail.com>
2019-05-14 21:57:58 -0500

    05-14-21:57:54.65

--- run.bat
+++ run.bat
@@ -1,1 +1,2 @@
 java -jar spigot.jar
+pause
---
+++ work/nms.old.1556855105622/minecraft/server/PlayerConnectionUtils.java
@@ -0,0 +1,18 @@
+package net.minecraft.server;
+
+public class PlayerConnectionUtils {
+
+    public static <T extends PacketListener> void ensureMainThread(Packet<T> packet, T t0, WorldServer worldserver) throws CancelledPacketHandleException {
+        ensureMainThread(packet, t0, (IAsyncTaskHandler) worldserver.getMinecraftServer());
+    }
+
+    public static <T extends PacketListener> void ensureMainThread(Packet<T> packet, T t0, IAsyncTaskHandler<?> iasynctaskhandler) throws CancelledPacketHandleException {
+        if (!iasynctaskhandler.isMainThread()) {
+            iasynctaskhandler.execute(() -> {
+                if (t0 instanceof PlayerConnection && ((PlayerConnection) t0).processedDisconnect) return; // CraftBukkit
+                packet.a(t0);
+            });
+            throw CancelledPacketHandleException.INSTANCE;
+        }
+    }
+}
